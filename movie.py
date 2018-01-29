0faa5e8f71e6a1639d401a950d903ed996857ee1
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-29 08:32:23 -0500

    Updated from Brython Server: 1/29/2018 8:32:23 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,15 @@
 
+#juliagolder
+#1/29/18
+#movie.py - how to calculate what movie you can watch
+
+num = float(input('Enter your age: '))
+
+if num >= 17:
+    print(num, 'You can watch: R')
+elif num >= 13:
+    print(num, 'You can watch: PG-13')
+elif num >= 10:
+    print(num, 'You can watch: PG')
+else:
+    print(num, 'You can watch: G!')
