fa8b4f4bba1c57ca309f4ec9e6ba87661eac2552
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-02-15 08:01:28 -0500

    Updated from Brython Server: 2/15/2018 8:01:27 AM

--- unitConverter.py
+++ unitConverter.py
@@ -11,18 +11,19 @@ print('4: Celsius to Fahrenheit')
 number = int(input('Choose a number: '))
 
 
-if number == 4:
+if number == 1:
     celcius = int(input('Enter Degrees in Celcius: '))
     fahrenheit = celcius * 1.8 + 32
     print(celcius,'degrees Celsius is', fahrenheit,'degres in Fahrenheit')
 elif number == 3:
-   liters = int(input('Enter amount in liters: '))
-    gallons = liters/3.785411784
-    print(liters,'liters is', gallons,' gallons')
- kilograms = int(input('Enter a number of Kilograms: '))
+    kilograms = int(input('Enter a number of Kilograms: '))
     pounds = kilograms/0.453592
     print(kilograms,'Kilograms is', pounds,'pounds')
-elif num == 1:
+elif number == 2:
+    liters = int(input('Enter amount in liters: '))
+    gallons = liters/3.785411784
+    print(liters,'liters is', gallons,' gallons')
+elif number == 4:
     kilometers = int(input('Enter a number of Kilometers'))
     miles = kilometers/1.60934
     print(kilometers,'kilometers is', miles,'miles')
