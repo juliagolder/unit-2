5b6e48670b79bfbd71a6a29e4ce30647bc094716
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-02-23 15:18:43 -0500

    Updated from Brython Server: 2/23/2018 3:18:43 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,20 @@
+#julia golder
+#2/23
+#insulter.py
 
+from random import randint
+
+input('Enter your name: ')
+
+num = randint(1,5)
+
+if num == 1:
+    print('If I had a face like yours, I'd sue my parents')
+elif num == 2:
+    print('')
+elif num == 3:
+    print('')
+elif num == 4:
+    print('')
+else:
+    print('')
