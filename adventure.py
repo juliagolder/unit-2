3e4c55c734ac83942d3148b455dfe2eba671bad2
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-02-24 10:20:41 -0500

    Updated from Brython Server: 2/24/2018 10:20:40 AM

--- adventure.py
+++ adventure.py
@@ -7,7 +7,7 @@ print('A bear is chasing you')
 print('You come to a river')
 choice1 = input('Do you jump in the river?')
 
-if choice1 == 'yes'
+if choice1 == 'y' or choice1 == 'yes':
     print('Yay! You know how to swim')
     print('Wait theres a giant fish coming!!')
     choice2 = input('Do you try to fight off the fish')
@@ -18,4 +18,4 @@ if choice1 == 'yes'
         print('Good choice! You out swam the fish')
         print('Looking back at the fish, there was a shark in front of you and now youre dead')
 else:
-    print('You keep running and you find a bridge!)
+    print('You keep running and you find a bridge!')
