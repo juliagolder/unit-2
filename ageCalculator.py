44f1352ed530478b01a92b44288a90a325c88c7b
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-03-20 07:32:40 -0400

    Create ageCalculator.py

---
+++ ageCalculator.py
@@ -0,0 +1,7 @@
+#Julia Golder
+#3/20/18
+#ageCalcualtor.py
+
+year = input('Enter the year you were born: ')
+month = input('Enter the month you were born: ')
+day = input('Enter the day you were born: ')
